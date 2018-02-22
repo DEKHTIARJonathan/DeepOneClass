0aa5bc427499aab8e89fcf1e5fc263f2c8148eea
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2018-02-22 17:36:28 +0100

    Unittests orchester added

---
+++ unittests.py
@@ -0,0 +1,26 @@
+import sys
+import unittest
+
+import tensorflow as tf
+import tensorlayer as tl
+
+sys.path.append("tests")
+
+from tests.test_model import OneClassCNNTest
+
+runner = unittest.TextTestRunner(verbosity=2)
+
+if __name__ == '__main__':
+
+    tf.logging.set_verbosity(tf.logging.INFO)
+    #tf.logging.set_verbosity(tf.logging.DEBUG)
+
+    for test_module in [OneClassCNNTest]:
+
+        tf.logging.info("Running tests for: %s ..." % test_module)
+
+        test_suite = unittest.TestLoader().loadTestsFromTestCase(test_module)
+        runner.run(test_suite)
+
+        tf.reset_default_graph()
+        tl.layers.clear_layers_name()
