633820fe188cfaf18e3b03feb22a0b38c120a6ad
milesial <milesial@users.noreply.github.com>
2018-06-10 22:14:06 +0200

    Fixed CM plot font colors

--- source_files/validation.py
+++ source_files/validation.py
@@ -29,7 +29,7 @@ def plot_confusion_matrix(cm, classes,
     plt.yticks(tick_marks, classes)
 
     fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
+    thresh = cm[0].sum() / 2.
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
         plt.text(j, i, format(cm[i, j], fmt),
                  horizontalalignment="center",
