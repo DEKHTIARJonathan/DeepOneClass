6fcbea0c0d322d299e2824c64d8985ddeed5cd20
milesial <milesial@users.noreply.github.com>
2018-06-30 18:43:46 +0200

    Standardization attempt

--- source_files/estimator_svdd.py
+++ source_files/estimator_svdd.py
@@ -67,7 +67,7 @@ def naive_svdd_model_fn(features, labels, mode, params):
             return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=metrics)
 
         elif mode == tf.estimator.ModeKeys.TRAIN:
-            lr = params["learning_rate"] if "q" in params else 0.1
+            lr = params["learning_rate"] if "learning_rate" in params else 0.1
             optimizer = tf.train.AdamOptimizer(lr)
 
             grads = optimizer.compute_gradients(loss, [R, a])
